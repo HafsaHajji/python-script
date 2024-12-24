fe0ae2f4150a9c777a315735e68e86b947b9cd8c
Hafsa Hassan <xafsaxaaji936@gmail.com>
2024-12-24 09:01:52 +0300

    this is python script in  alogorithm

---
+++ main.py
@@ -0,0 +1,48 @@
+# This is a sample Python script.
+
+# Press Shift+F10 to execute it or replace it with your code.
+# Press Double Shift to search everywhere for classes, files, tool windows, actions, and settings.
+
+
+#def print_hi(name):
+    # Use a breakpoint in the code line below to debug your script.
+   # print(f'Hi, {name}')  # Press Ctrl+F8 to toggle the breakpoint.
+
+
+# Press the green button in the gutter to run the script.
+#if __name__ == '__main__':
+ #   print_hi('PyCharm')
+
+# See PyCharm help at https://www.jetbrains.com/help/pycharm/
+
+#odd practice
+#num=7
+#if num % 2 == 0:
+ #   print("even")
+
+#else:
+   # print("odd")
+
+
+#even practice
+#num = 20
+#if num % 2 == 0:
+ #   print("even")
+
+#else:
+ #   print("odd")
+
+
+#SIMPLE ALOGROTHM
+
+
+
+  #BIG O NOTAION
+
+#a=[4,0,10,8,9,20]
+#n=0
+#For (i=0; i<n; i++);
+  #  (
+     #
+   # )
+
